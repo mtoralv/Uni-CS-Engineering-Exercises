4867e592f9992e44014cf43bb913334faf4e6dc3
Marcos Torres Álvarez <marcostorresalvarez007@gmail.com>
2026-02-12 03:32:32 +0100

    Changed name in Year_01 dir to fix error with Java packages

---
+++ Year_01/Semester_01_C++/05_FinalPractice/02_221225/09_LookAndSay.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string lookandsay(string seed);
+   
+    cout <<lookandsay("1") << endl;
+    
+return 0;
+}
+
+string lookandsay(string seed)
+{
+    
+    for( int i=0; i<seed.size();i++)
+    {
+        
+    }  
+    
+}
+
