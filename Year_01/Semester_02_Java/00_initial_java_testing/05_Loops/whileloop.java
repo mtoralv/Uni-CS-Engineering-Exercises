a3e45ad2d6c77a69870dee6e55dcd03524ee0690
Marcos Torres Álvarez <211049417+mtoralv@users.noreply.github.com>
2026-02-24 11:10:22 +0100

    Changed dir naming

---
+++ Year_01/Semester_02_Java/00_initial_java_testing/05_Loops/whileloop.java
@@ -0,0 +1,17 @@
+public class whileloop {
+    public static void main(String[] args) {
+     
+        int i=0;
+        while(i<10)
+        {
+            System.out.println("JAVA!");
+            i++;
+        }
+
+        do {
+            System.out.println("Hello, Java!");
+            i--;
+        } while (i > 0);
+
+    }    
+}
